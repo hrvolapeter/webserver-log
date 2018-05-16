b47cb0193bdf9dce789c65e0493b1daddb31e87e
Travis <travis@Traviss-Mac-463.local>
2018-05-16 09:49:24 +0000

    Automatic Travis documentation build

---
+++ master/implementors/webserver_log_lib/input/trait.Input.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["webserver_log_lib"] = [];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ master/webserver_log_lib/input/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"mod":[["apache","Input from Apache logs"],["elasticsearch","Input from Elasticsearch"]],"trait":[["Input","All new log sources at this layer must implement this trait. It allows to use the new source to get logs. Logs are beeing stored in heap."]]});
